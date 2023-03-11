c2c6ead712ffb41b8552d5d4dcda8b6056ea7b77
pazrein <reingoldpaz@gmail.com>
2023-03-11 18:14:44 +0200

    Complete Messages Project

--- messages/src/messages/messages.module.ts
+++ messages/src/messages/messages.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MessagesController } from './messages.controller';
+import { MessagesService } from './messages.service';
+import {MessagesRepository} from "./messages.repository";
 
 @Module({
-  controllers: [MessagesController]
+  controllers: [MessagesController],
+  providers: [MessagesService, MessagesRepository]
 })
 export class MessagesModule {}
