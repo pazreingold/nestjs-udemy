cc0cd6f311853f5207b89edf1324c9ea815015a0
pazrein <reingoldpaz@gmail.com>
2023-03-14 22:21:09 +0200

    Configure TypeOrm - Use Sql Lite as Database

--- car-value/src/app.module.ts
+++ car-value/src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [UsersModule, ReportsModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [],
+      synchronize: true
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
