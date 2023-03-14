5afac7e12eb39e08ceba187485cf0495541ea8fe
pazrein <reingoldpaz@gmail.com>
2023-03-14 19:09:27 +0200

    Nest Generate

--- car-value/src/app.module.ts
+++ car-value/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService],
 })
