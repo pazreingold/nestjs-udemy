60ed843ad6b1b6b927e614c587dbe482202088cd
pazrein <reingoldpaz@gmail.com>
2023-03-22 23:38:24 +0200

    Remove Comment

--- car-value/src/users/users.controller.ts
+++ car-value/src/users/users.controller.ts
@@ -28,11 +28,6 @@ export class UsersController {
         private authService: AuthService) {
     }
 
-    // @Get('/whoami')
-    // whoAmI(@Session() session: any) {
-    //     return this.userService.findOne(session.userId);
-    // }
-
     @Get('/whoami')
     @UseGuards(AuthGuard)
     whoAmI(@CurrentUser() user: User) {
