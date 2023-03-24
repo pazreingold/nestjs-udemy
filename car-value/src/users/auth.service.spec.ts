acbbaa3b8b42b05084997ea018246e238a54b0d8
pazrein <reingoldpaz@gmail.com>
2023-03-24 18:32:04 +0300

    Test an Error is Thrown if Email Exists When Signup

--- car-value/src/users/auth.service.spec.ts
+++ car-value/src/users/auth.service.spec.ts
@@ -2,12 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import {UsersService} from "./users.service";
 import {User} from "./user.entity";
+import {BadRequestException} from "@nestjs/common";
 
 describe('AuthService', () => {
   let service: AuthService;
+  let fakeUserService: Partial<UsersService>;
 
   beforeEach(async () => {
-      const fakeUserService: Partial<UsersService> = {
+      fakeUserService = {
           find: () => Promise.resolve([]),
           create: (email: string, password: string) =>
               Promise.resolve({id: 1, email, password} as User),
@@ -34,5 +36,11 @@ describe('AuthService', () => {
       const[salt, hash] = user.password.split('.');
       expect(salt).toBeDefined();
       expect(hash).toBeDefined();
-  })
+  });
+
+    it('throws an error if user signs up with email that already exists', async () => {
+        fakeUserService.find = () =>
+            Promise.resolve([{id: 1, email: 'test@test.com', password: 'test'} as User]);
+        await expect(service.signup('test@test.com', 'test')).rejects.toThrow(BadRequestException);
+    });
 });
