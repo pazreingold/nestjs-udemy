c474c35fb562946360883736d8a54bf6c85bf73a
pazrein <reingoldpaz@gmail.com>
2023-03-24 16:55:21 +0300

    Make Test Pass

--- car-value/src/users/auth.service.spec.ts
+++ car-value/src/users/auth.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import {UsersService} from "./users.service";
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
+      const fakeUserService = {
+          find: () => Promise.resolve([]),
+          create: (email: string, password: string) =>
+              Promise.resolve({id: 1, email, password}),
+      }
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [AuthService,
+          {
+              provide: UsersService,
+              useValue: fakeUserService,
+          }],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
