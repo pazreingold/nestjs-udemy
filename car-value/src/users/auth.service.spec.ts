a490610519d16b59ff6549020bf047b701ceb66f
pazrein <reingoldpaz@gmail.com>
2023-03-24 16:56:13 +0300

    Add Types to User Service in Auth Service Test

--- car-value/src/users/auth.service.spec.ts
+++ car-value/src/users/auth.service.spec.ts
@@ -1,15 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import {UsersService} from "./users.service";
+import {User} from "./user.entity";
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
-      const fakeUserService = {
+      const fakeUserService: Partial<UsersService> = {
           find: () => Promise.resolve([]),
           create: (email: string, password: string) =>
-              Promise.resolve({id: 1, email, password}),
+              Promise.resolve({id: 1, email, password} as User),
       }
     const module: TestingModule = await Test.createTestingModule({
       providers: [AuthService,
