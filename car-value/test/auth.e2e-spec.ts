0e5ecbd0defae6cb3f2a9d6902504469433dd87b
pazrein <reingoldpaz@gmail.com>
2023-03-31 14:16:14 +0300

    Test The User is Currently Logged-In

--- car-value/test/auth.e2e-spec.ts
+++ car-value/test/auth.e2e-spec.ts
@@ -27,4 +27,22 @@ describe('Authentication System', () => {
                 expect(email).toBe(email);
             });
     });
+
+    it('signup as a new user then get the currently logged in user', async () => {
+        const email = "test@test.com";
+
+        const res = await request(app.getHttpServer())
+            .post('/auth/signup')
+            .send({email, password: 'test'})
+            .expect(201);
+
+        const cookie = res.get('Set-Cookie');
+
+        const { body } = await request(app.getHttpServer())
+            .get('/auth/whoami')
+            .set('Cookie', cookie)
+            .expect(200);
+
+        expect(body.email).toBe(email);
+    });
 });
