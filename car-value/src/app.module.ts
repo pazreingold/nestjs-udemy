0209f4870abcc16e3c967ff0be2248f8deb77146
pazrein <reingoldpaz@gmail.com>
2023-03-31 13:22:59 +0300

    Configure Different Databases for Test & Development

--- car-value/src/app.module.ts
+++ car-value/src/app.module.ts
@@ -7,16 +7,27 @@ import {TypeOrmModule} from "@nestjs/typeorm";
 import {User} from "./users/user.entity";
 import {Report} from "./reports/report.entity";
 import {APP_PIPE} from "@nestjs/core";
+import {ConfigModule, ConfigService} from "@nestjs/config";
 const cookieSession = require('cookie-session');
 
 @Module({
     imports: [UsersModule, ReportsModule,
-        TypeOrmModule.forRoot({
-            type: 'sqlite',
-            database: 'db.sqlite',
-            entities: [User, Report],
-            synchronize: true
-        })],
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: `.env.${process.env.NODE_ENV}`,
+        }),
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => {
+                return {
+                    type: 'sqlite',
+                    database: config.get<string>('DB_NAME'),
+                    synchronize: true,
+                    entities: [User, Report],
+                };
+            },
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService,
         {
