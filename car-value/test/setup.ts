0f518c07fea6ee8a790ee6a8db5967d3014ea546
pazrein <reingoldpaz@gmail.com>
2023-03-31 13:51:21 +0300

    Remove test.sqlite Before Every Test
    In order to create the db before every test, and not getting 'already created' error

---
+++ car-value/test/setup.ts
@@ -0,0 +1,8 @@
+import {rm} from "fs/promises";
+import {join} from "path";
+
+global.beforeEach(async () => {
+    try {
+        await rm(join(__dirname, '..', 'test.sqlite'));
+    } catch (err) {}
+});
