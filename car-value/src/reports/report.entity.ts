a64684e7c185ce572676373730bc7e161bd8fdb5
pazrein <reingoldpaz@gmail.com>
2023-04-01 11:03:52 +0300

    Add Many-to-One/One-to-Many Association Between Report and User

--- car-value/src/reports/report.entity.ts
+++ car-value/src/reports/report.entity.ts
@@ -1,4 +1,5 @@
-import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+import {Column, Entity, ManyToOne, PrimaryGeneratedColumn} from "typeorm";
+import {User} from "../users/user.entity";
 
 @Entity()
 export class Report {
@@ -25,4 +26,7 @@ export class Report {
 
     @Column()
     mileage: number
+
+    @ManyToOne(() => User, (user) => user.reports)
+    user: User;
 }
--- car-value/src/users/user.entity.ts
+++ car-value/src/users/user.entity.ts
@@ -1,4 +1,5 @@
-import {AfterInsert, AfterRemove, AfterUpdate, Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+import {AfterInsert, AfterRemove, AfterUpdate, Column, Entity, OneToMany, PrimaryGeneratedColumn} from "typeorm";
+import {Report} from "../reports/report.entity";
 
 @Entity()
 export class User {
@@ -10,6 +11,10 @@ export class User {
 
     @Column()
     password: string;
+
+    @OneToMany(() => Report, (report) => report.user)
+    reports: Report[];
+
 
     @AfterInsert()
     logInsert() {
