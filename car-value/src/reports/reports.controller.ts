c2a57da6dc94073282f40ed640f5575d94cd61eb
pazrein <reingoldpaz@gmail.com>
2023-04-05 14:59:32 +0300

    Fix Import

--- car-value/src/reports/reports.controller.ts
+++ car-value/src/reports/reports.controller.ts
@@ -1,5 +1,4 @@
 import {Body, Controller, Get, Param, Patch, Post, Query, UseGuards} from '@nestjs/common';
-import {AuthGuard} from 'src/guards/auth.guard';
 import {CreateReportDto} from "./dtos/create-report.dto";
 import {ReportsService} from "./reports.service";
 import {User} from "../users/user.entity";
@@ -9,6 +8,7 @@ import {Serialize} from "../interceptors/serialize.interceptors";
 import {ApprovedReportDto} from "./dtos/approved-report.dto";
 import {AdminGuard} from "../guards/admin.guard";
 import {GetEstimateDto} from "./dtos/get-estimate.dto";
+import {AuthGuard} from "../guards/auth.guard";
 
 @Controller('reports')
 export class ReportsController {
